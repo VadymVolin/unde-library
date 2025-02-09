e010656d554a15512db790a3523f83879ef83fa7
Vadym Volin <vadim.volin1@gmail.com>
2025-02-09 21:24:36 +0200

    Init library project

---
+++ lib/src/main/kotlin/com/unde/library/UndeLibrary.kt
@@ -0,0 +1,4 @@
+package com.unde.library
+
+class UndeLibrary {
+}
---
+++ lib/src/test/kotlin/com/unde/library/UndeLibraryTest.kt
@@ -0,0 +1,11 @@
+package com.unde.library
+
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class UndeLibraryTest {
+    @Test fun someLibraryMethodReturnsTrue() {
+        val classUnderTest = UndeLibrary()
+        assertTrue(true, "someLibraryMethod should return 'true'")
+    }
+}
