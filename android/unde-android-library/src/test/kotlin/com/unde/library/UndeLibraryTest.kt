8fa7b9517cb5ec77e86e536b4d90f973ed393641
Vadym Volin <vadim.volin1@gmail.com>
2025-09-27 14:53:22 +0300

    Modified test code

--- android/unde-android-library/src/test/kotlin/com/unde/library/UndeLibraryTest.kt
+++ android/unde-android-library/src/test/kotlin/com/unde/library/UndeLibraryTest.kt
@@ -1,51 +1,59 @@
 package com.unde.library
 
-import com.unde.library.external.network.interceptor.ktor.UndeHttpInterceptor as KtorInterceptor
 import com.unde.library.external.network.interceptor.okhttp.UndeHttpInterceptor
 import com.unde.library.internal.constants.DEFAULT_CONNECTION_TIMEOUT_SEC
 import com.unde.library.internal.constants.DEFAULT_PING_INTERVAL_SEC
-import io.ktor.client.*
-import io.ktor.client.call.*
-import io.ktor.client.engine.okhttp.*
-import io.ktor.client.plugins.*
-import io.ktor.client.plugins.websocket.*
-import io.ktor.client.request.*
-import io.ktor.serialization.kotlinx.*
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.HttpSend
+import io.ktor.client.plugins.plugin
+import io.ktor.client.plugins.websocket.WebSockets
+import io.ktor.client.request.get
+import io.ktor.serialization.kotlinx.KotlinxWebsocketSerializationConverter
 import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.json.Json
 import okhttp3.OkHttpClient
-import org.junit.Before
+import org.junit.BeforeClass
 import org.junit.Test
 import java.time.Duration
 import kotlin.time.Duration.Companion.seconds
+import com.unde.library.external.network.interceptor.ktor.UndeHttpInterceptor as KtorInterceptor
 
 class UndeLibraryTest {
 
-    val undeLibrary = UndeLibrary()
+    companion object {
+        @get:BeforeClass
+        val undeLibrary = UndeLibrary()
 
-    val okHttpClient = HttpClient(OkHttp) {
-        engine {
-            // The pingInterval and pingIntervalMillis properties are not applicable for the OkHttp engine
-            //  https://ktor.io/docs/client-websockets.html#configure_plugin
-            preconfigured = OkHttpClient.Builder()
-                .pingInterval(Duration.ofSeconds(DEFAULT_PING_INTERVAL_SEC))
-                .connectTimeout(Duration.ofSeconds(DEFAULT_CONNECTION_TIMEOUT_SEC))
-                .addNetworkInterceptor(UndeHttpInterceptor())
-                .build()
+        @get:BeforeClass
+        val okHttpClient = HttpClient(OkHttp) {
+            engine {
+                // The pingInterval and pingIntervalMillis properties are not applicable for the OkHttp engine
+                //  https://ktor.io/docs/client-websockets.html#configure_plugin
+                preconfigured = OkHttpClient.Builder()
+                    .pingInterval(Duration.ofSeconds(DEFAULT_PING_INTERVAL_SEC))
+                    .connectTimeout(Duration.ofSeconds(DEFAULT_CONNECTION_TIMEOUT_SEC))
+                    .addNetworkInterceptor(UndeHttpInterceptor())
+                    .build()
+            }
+            install(WebSockets) {
+                contentConverter = KotlinxWebsocketSerializationConverter(Json)
+                maxFrameSize = Long.MAX_VALUE
+            }
         }
-        install(WebSockets) {
-            contentConverter = KotlinxWebsocketSerializationConverter(Json)
-            maxFrameSize = Long.MAX_VALUE
-        }
+
+        @get:BeforeClass
+        val interceptor = KtorInterceptor()
+        @get:BeforeClass
+        val ktorHttpClient = HttpClient(OkHttp) {
+            install(WebSockets) {
+                contentConverter = KotlinxWebsocketSerializationConverter(Json)
+                maxFrameSize = Long.MAX_VALUE
+            }
+        }.apply { plugin(HttpSend).intercept(interceptor::invoke) }
     }
 
-    val interceptor = KtorInterceptor()
-    val ktorHttpClient = HttpClient(OkHttp) {
-        install(WebSockets) {
-            contentConverter = KotlinxWebsocketSerializationConverter(Json)
-            maxFrameSize = Long.MAX_VALUE
-        }
-    }.apply { plugin(HttpSend).intercept(interceptor::invoke) }
 
     @Test
     fun checkOkhttpRequestInterceptor() {
